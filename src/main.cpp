8af16e24ae2fb292728b1b1ca915d4ec1aba0401
Geoff Lindsell <geoffreyjohnwoodslindsell@gmail.com>
2025-02-08 09:44:31 +0000

    Updated insertCurrency and containsCurrency in Wallet.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include "CSVReader.hpp"
 #include "OrderBookEntry.hpp"
 #include "MerkleMain.hpp"
+#include "Wallet.hpp"
+#include <iostream>
 
 int main()
 {
-    MerkleMain app{};
-    app.init();
+    // MerkleMain app{};
+    // app.init();
+    Wallet wallet;
+    wallet.insertCurrency("BTC", 10);
+    std::cout << "Wallet has BTC " << wallet.containsCurrency("BTC", 10) << std::endl;
+    std::cout << wallet.toString() << std::endl;
 }
