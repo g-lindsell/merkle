09f1152fa28fec0355f0075ab76b8cd42aa6c2b1
Geoff Lindsell <geoffreyjohnwoodslindsell@gmail.com>
2025-02-11 07:21:31 +0000

    Added canFulfillOrder method to Wallet class

--- src/Wallet.hpp
+++ src/Wallet.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <map>
 #include <iostream>
+#include "OrderBookEntry.hpp"
 
 class Wallet
 {
@@ -10,11 +11,16 @@ public:
     Wallet();
     /** insert currency to the wallet */
     void insertCurrency(std::string type, double amount);
+
     /** remove currency to the wallet */
     bool removeCurrency(std::string type, double amount);
 
     /** check if the wallet contains this much currency or more */
     bool containsCurrency(std::string type, double amount);
+
+    /** checks if the wallet can cope with this ask or bid. */
+    bool canFulfillOrder(OrderBookEntry order);
+
     /** generate a string representation of the wallet */
     std::string toString();
 
